6ed90087bdb1bcc3da62eb29fb70f21fa58a0641
Afolabi Rasheed <aforasheed385@gmail.com>
2022-09-28 23:04:36 -0700

    correct toggle

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,16 @@
-const toggleMenu = document.getElementsByClassName('menu');
-const toggleLinks = document.getElementsByClassName('links');
-
+const toggleMenu = document.getElementById('menus');
+const toggleLinks = document.getElementById('link');
+const toggleClose = document.getElementById('close');
 
 if(toggleMenu){
     toggleMenu.addEventListener('click', () => {
         toggleLinks.classList.add('active')
         // console.log("rad")
+    })
+}
+
+if(toggleClose){
+    toggleClose.addEventListener('click', () => {
+        toggleLinks.classList.remove('active')
     })
 }
